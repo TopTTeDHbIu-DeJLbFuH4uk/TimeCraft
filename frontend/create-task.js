b5a3dbde4b8eda396983ee9fb86c4f0da1085fd2
LaProfessional <118113982+LaProfessional@users.noreply.github.com>
2025-02-04 17:11:44 +0700

    04.02.2025 17:12

--- frontend/create-task.js
+++ frontend/create-task.js
@@ -1,53 +1,56 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const nameTaskFieldEl = document.querySelector('.name-task-field');
-//     const descriptionFieldEl = document.querySelector('.description-field');
-//
-//     const startDateEl = document.querySelector('.start-date');
-//     const startTimeEl = document.querySelector('.start-time');
-//
-//     const endDateEl = document.querySelector('.end-date');
-//     const endTimeEl = document.querySelector('.end-time');
-//
-//     const contentInInputsEls = [...document.querySelectorAll('.content-in-input')];
-//
-//     const btnSaveEl = document.querySelector('.btn-save');
-//     const btnCancelEl = document.querySelector('.btn-cancel');
-//
-//     const tableBodyEl = document.querySelector('.table-body');
-//     console.log(tableBodyEl); // null
-//
-//     const currentDate = document.querySelector('.start-date');
-//     currentDate.valueAsDate = new Date();
-//
-//     const currentTime = document.querySelector('.start-time');
-//     const now = new Date();
-//     const hours = now.getHours().toString().padStart(2, '0');
-//     const minutes = now.getMinutes().toString().padStart(2, '0');
-//     currentTime.value = `${hours}:${minutes}`;
-//
-//     const createTask = () => {
-//         const nameTask = nameTaskFieldEl.value;
-//         const description = descriptionFieldEl.value;
-//         const startDate = startDateEl.value;
-//         const endDate = endDateEl.value;
-//
-//         const tr = document.createElement('tr');
-//         const th = document.createElement('th');
-//         const input = document.createElement('input');
-//
-//         // tableBodyEl.append(tr);
-//         // tr.append(th);
-//
-//         console.log('sdf')
-//     };
-//
-//
-//     btnSaveEl.addEventListener('click', () => {
-//
-//         const checkInputValue = contentInInputsEls.every(el => el.value);
-//
-//         if (checkInputValue) {
-//             createTask();
-//         }
-//     });
-// });
+const titleEl = document.getElementById('title');
+const descriptionEl = document.getElementById('description');
+
+const startDateEl = document.getElementById('start-date');
+const startTimeEl = document.getElementById('start-time');
+
+const endDateEl = document.getElementById('end-date');
+const endTimeEl = document.getElementById('end-time');
+
+const inputEls = [...document.querySelectorAll('.input')];
+
+const btnSaveEl = document.querySelector('.btn-save');
+const btnCancelEl = document.querySelector('.btn-cancel');
+
+const createTask = () => {
+
+    const task = {
+        name: '',
+        description: '',
+        startDate: '',
+        startTime: '',
+        endDate: '',
+        endTime: '',
+    };
+
+    const inputValues = inputEls.map(inputEl => {
+        return inputEl.value;
+    });
+
+    Object.keys(task).forEach((key, index) => {
+        task[key] = inputValues[index];
+    });
+    database.push(task);
+    console.log(database);
+
+
+    resetInputs();
+};
+
+btnSaveEl.addEventListener('click', () => {
+    const checkInputValue = inputEls.every(el => el.value);
+
+    if (checkInputValue) {
+        createTask();
+    }
+});
+
+const resetInputs = () => {
+    inputEls.forEach(inputEl => inputEl.value = '');
+};
+
+startDateEl.valueAsDate = new Date();
+const now = new Date();
+const hours = now.getHours().toString().padStart(2, '0');
+const minutes = now.getMinutes().toString().padStart(2, '0');
+startTimeEl.value = `${hours}:${minutes}`;
