2a80c0744fd11fd81d7d8855803a360dcfc1a663
LaProfessional <118113982+LaProfessional@users.noreply.github.com>
2025-02-08 13:39:36 +0700

    08.02.2025 13:40

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,38 @@
 import express from 'express';
 import path from 'path';
 
-const PORT = 5000;
+const PORT = 3000;
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.json());
+
+const tasks = [];
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('public/tasks.html'));
+});
+
+app.get('/tasks', (req, res) => {
+    res.json(tasks);
+});
 
 app.get('/task-create', (req, res) => {
-    res.sendFile(path.resolve('public/task-create.html'), (err) => {
-        if (err) {
-            console.log(path.resolve('public/task-create.html'));
-            res.status(404).send('Файл не найден');
-        }
-    });
+    res.sendFile(path.resolve('public/task-create.html'));
 });
 
 app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}`));
+
+app.post('/task-create', (req, res) => {
+    req.body = {
+        task: {
+            ...req.body,
+            creationDatetime: new Date().toISOString(),
+            id: tasks.length + 1,
+        }
+    };
+
+    tasks.push(req.body);
+
+    res.status(200).json(req.body);
+});
