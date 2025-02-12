83ca4980e1d12bff8fcaae97be52a0e0824da38c
LaProfessional <118113982+LaProfessional@users.noreply.github.com>
2025-02-13 00:26:47 +0700

    13.02.2025 00:27

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
 import express from 'express';
 import path from 'path';
+import pkg from 'pg';
 
-// const {Client} = require('pg');
-// const client = new Client({
-//     user: 'TopTTeDHbIu-DeJLbFuH4uk',
-//     host: 'localhost',
-//     database: 'tasks',
-//     password: 'qwerty321',
-//     port: 5432,
-// });
-// client.connect();
+const {Pool} = pkg;
+
+const pool = new Pool({
+    user: 'TopTTeDHbIu-DeJLbFuH4uk',
+    host: 'localhost',
+    database: 'tasks',
+    password: 'qwerty321',
+    port: 5432,
+});
+pool.connect()
+    .then(client => {
+        return client.query('SELECT 1 FROM tasks LIMIT 1;')
+            .then(() => {
+                console.log('Connected to PostgreSQL and table "tasks" is accessible');
+                client.release();
+            })
+            .catch(err => {
+                console.error('Error accessing "tasks" table:', err.stack);
+                client.release();
+            });
+    })
+    .catch(err => {
+        console.error('Connection error', err.stack);
+    });
 
 const PORT = 3000;
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.json());
-
-let tasks = [];
-let taskIds = [];
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('public/tasks.html'));
@@ -33,19 +46,24 @@ app.get('/task-create', (req, res) => {
 });
 
 app.post('/task-create', (req, res) => {
-    const addTimeAndIdToTask = {
-        creationDatetime: new Date().toISOString(),
-        id: taskIds.length + 1,
-    }
 
-    const task = {
-        ...req.body.task,
-        ...addTimeAndIdToTask,
-    }
-    tasks.push(task);
-    taskIds.push(null);
+    const start_datetime = {
+        start_datetime: `${req.body.task.startDate} ${req.body.task.startTime}`,
+    };
+    const end_datetime = {
+        end_datetime: `${req.body.task.endDate} ${req.body.task.endTime}`,
+    };
 
-    res.status(200).json(addTimeAndIdToTask);
+    try {
+        const result = pool.query(`INSERT INTO tasks (title, description, start_datetime, end_datetime)
+                                   VALUES ($1, $2, $3, $4) RETURNING id`,
+            [req.body.task.title, req.body.task.description, start_datetime, end_datetime]
+        );
+        res.status(201).json(result);
+
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 app.delete('/tasks', (req, res) => {
@@ -53,7 +71,7 @@ app.delete('/tasks', (req, res) => {
 
     selectedTaskIds.forEach(id => {
         const index = tasks.findIndex(task => task.id === id);
-        if (index !== -1) {
+        if (index !== -1) { // ?
             tasks.splice(index, 1);
         }
     });
